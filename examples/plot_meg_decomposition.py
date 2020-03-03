670e3bc9f2f4507cca820db5cab216cfd555d18c
Pierre Ablin <pierreablin@gmail.com>
2020-03-03 01:08:09 +0100

    update

--- examples/plot_meg_decomposition.py
+++ examples/plot_meg_decomposition.py
@@ -1,144 +1,142 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from smica import ICA
+from smica import ICA, mutual_information_2d
 import mne
 from mne.datasets import sample
+from picard import picard
+
+from joblib import Memory
+
+location = './cachedir'
+memory = Memory(location, verbose=0)
+EPS = 1e-12
 
 # fetch data
 data_path = sample.data_path()
 raw_fname = data_path + '/MEG/sample/sample_audvis_raw.fif'
 event_fname = data_path + '/MEG/sample/sample_audvis_filt-0-40_raw-eve.fif'
 raw = mne.io.read_raw_fif(raw_fname, preload=True)
-T = 20
-raw.crop(0, T)
+# T = 20
+# raw.crop(0, T)
 picks = mne.pick_types(raw.info, meg='mag', eeg=False, eog=False,
                        stim=False, exclude='bads')
 
-# Compute ICA on raw: chose the frequency decomposition. Here, uniform between
-# 2 - 35 Hz.
-n_bins = 10
+n_bins = 20
 freqs = np.linspace(2, 35, n_bins + 1)
-# n_comp = 20
+
+raw.filter(2, 35)
+n_comp = 20
 # smica = ICA(n_components=n_comp, freqs=freqs, rng=0)
-# smica.fit(raw, picks=picks, corr=True, verbose=100, tol=1e-7, em_it=10000)
-# print(smica.compute_loss() / n_bins)
-# losses = []
-n_comps = list(range(1, 50)) + [60, 70, 80, 90, 100]
-
-# n_comp = 10
-# smica = ICA(n_components=n_comp, freqs=freqs, rng=0)
-# smica.fit(raw, picks=picks, verbose=100, tol=1e-7, em_it=10000)
-# smica2 = ICA(n_components=n_comp, freqs=freqs, rng=0)
-# smica2.fit(raw, picks=picks, corr=True, verbose=1, tol=1e-7, em_it=10000)
-losses = []
-for n_comp in n_comps:
-    # print('SMICA uncorr')
-    # smica = ICA(n_components=n_comp, freqs=freqs, rng=0)
-    # smica.fit(raw, picks=picks, verbose=100, tol=1e-7, em_it=10000)
-
-    print(n_comp)
-
-    smica2 = ICA(n_components=n_comp, freqs=freqs, rng=0)
-    smica2.fit(raw, picks=picks, corr=True, verbose=100, tol=1e-7, em_it=10000)
-    losses.append(smica2.compute_loss())
-# baseline = smica2.smica.baseline_loss
-# losses.insert(0, (baseline, baseline))
-f, ax = plt.subplots()
-plt.plot(n_comps, losses)
-# plt.plot(n_comps, [loss[1] for loss in losses])
-plt.yscale('log')
-plt.show()
-# C = smica.smica.C_
-# Cs = smica.smica.compute_approx_covs()
-# Cs2 = smica2.smica.compute_approx_covs()
-# smica_loss = smica.compute_loss(by_bin=True)
-# smica2_loss = smica2.compute_loss(by_bin=True)
-# f, ax = plt.subplots(3, n_bins, figsize=(30, 7))
-# for i in range(n_bins):
-#     for j, c in enumerate([C[i], Cs2[i], Cs[i]]):
-#         axe = ax[j, i]
-#         axe.matshow(c)
-#         axe.set_xticks([])
-#         axe.set_yticks([])
-#         if j == 0:
-#             if i == 0:
-#                 x_ = axe.set_ylabel('Covs')
-#             axe.set_title('%d - %d Hz' % (freqs[i], freqs[i+1]))
-#         if j == 1:
-#             if i == 0:
-#                 axe.set_ylabel('Subspace')
-#             axe.set_title('loss=%.1f' % smica2_loss[i])
-#         if j == 2:
-#             if i == 0:
-#                 axe.set_ylabel('SMICA')
-#         print(n_comp)        axe.set_title('loss=%.1f' % smica_loss[i])
-# t_ = f.suptitle('%d components' % n_comp)
-# # f.tight_layout()
-# plt.savefig('covs_%d.png' % n_comp, bbox_extra_artists=[t_, x_],
-#             bbox_inches='tight')
-# plt.show()
-
-#
-# f, ax = plt.subplots(2, n_bins, figsize=(30, 7))
-# for i in range(n_bins):
-#     for j, c in enumerate([Cs2[i], Cs[i]]):
-#         axe = ax[j, i]
-#         axe.matshow(C[i].dot(np.linalg.pinv(c)))
-#         axe.set_xticks([])
-#         axe.set_yticks([])
-#         if j == 0:
-#             if i == 0:
-#                 x_ = axe.set_ylabel('Subspace')
-#             axe.set_title('KL=%.1f' % smica2_loss[i])
-#         if j == 1:
-#             if i == 0:
-#                 axe.set_ylabel('SMICA')
-#             axe.set_title('KL=%.1f' % smica_loss[i])
-# t_ = f.suptitle('%d components' % n_comp)
-# # f.tight_layout()
-# plt.savefig('covs_invs_%d.png' % n_comp, bbox_extra_artists=[t_, x_],
-#             bbox_inches='tight')
-# plt.show()
+# smica.fit(raw, picks=picks, verbose=100, tol=1e-7, em_it=10000, corr=True)
+# # P = smica.powers
+# # eigs = np.array([np.linalg.eigvalsh(p) for p in P])
+# # plt.semilogy(eigs.T[::-1])
+# # plt.show()
+# S = smica.compute_sources()
 
 
-def extract(C, i, j):
-    op = [[C[:, i, i], C[:, i, j]], [C[:, j, i], C[:, j, j]]]
-    op = np.array(op).T
-    return op
+def getent2(u, nbins=None):
+
+    """
+    This is a python translation of the getent2.m matlab function, which
+    is included in the code realeased with the plosone paper
+    "EEG independent sources are dipolar"
+    """
+    nu, Nu = u.shape
+
+    if nbins is None:
+        nbins = np.min([100, np.round(np.sqrt(Nu))])
+
+    Hu = np.zeros(nu)
+    deltau = np.zeros(nu)
+
+    for i in range(nu):
+
+        umax = np.max(u[i])
+        umin = np.min(u[i])
+
+        deltau[i] = (umax - umin) / float(nbins)
+
+        uu = \
+            1 + np.round((nbins - 1) * (u[i] - umin) / float(umax - umin))
+
+        temp = np.nonzero(np.diff(np.sort(uu)))[0] + 1
+
+        ##############################################################
+        # pmfr = np.diff(np.append(np.append(0, temp), Nu)) / float(Nu)
+
+        temp2 = np.zeros(len(temp) + 2)
+        temp2[-1] = Nu
+        temp2[1:-1] = temp
+        pmfr = np.diff(temp2) / float(Nu)
+
+        ##############################################################
+
+        Hu[i] = -np.sum(pmfr * np.log(pmfr)) + np.log(deltau[i])
+
+    return Hu, deltau
 
 
-def compute_dist_sensors(smica):
-    C = smica.smica.C_
-    C_s = smica.smica.compute_approx_covs()
-    _, p, _ = C.shape
-    D = np.zeros((p, p))
-    for i in range(p):
+def get_mir(estimated_sources, sfreq=1000):
+
+    h0 = 0
+    h, _ = getent2(estimated_sources)
+
+    # Let's transform nats to kbits per second
+    # 1 nat = log2(exp(1)) bits
+    # EEG.srate is the sampling frequency
+    # 1kbit = 1000 bits
+    C = np.dot(estimated_sources, estimated_sources.T)
+    mir = (- h + .5 * np.linalg.slogdet(C)[1] / len(h)) * \
+        np.log2(np.exp(1)) * sfreq / 1000.
+
+    # mir is expressed in kbits / seconds
+    return mir
+
+
+def get_mi(S):
+    n_sources = S.shape[0]
+    MI = np.zeros((n_sources, n_sources))
+    for i in range(n_sources):
         for j in range(i):
-            d = np.sum([KL(A, B)
-                        for A, B in zip(extract(C, i, j), extract(C_s, i, j))])
-            D[i, j] = d
-            D[j, i] = d
-    return D
+            mi = mutual_information_2d(S[i], S[j], 1, False)
+            MI[i, j] = mi
+            MI[j, i] = mi
+    return MI
 
 
-def KL(A, B):
-    p, _ = A.shape
-    D = A.dot(np.linalg.pinv(B))
-    return np.trace(D) - np.linalg.slogdet(D)[1] - p
-# # Plot the powers
-#
-# smica.plot_powers()
+@memory.cache()
+def get_sources(n_comp):
+    #
+    smica = ICA(n_components=n_comp, freqs=freqs, rng=0)
+    smica.fit(raw, picks=picks, tol=1e-7, em_it=10000, corr=True)
+    # P = smica.powers
+    # eigs = np.array([np.linalg.eigvalsh(p) for p in P])
+    # plt.semilogy(eigs.T[::-1])
+    # plt.show()
+    S = smica.compute_sources()
+    _, _, S_smica = picard(S, ortho=False, extended=False, max_iter=1000)
+    _, _, S_infomax = picard(raw.get_data(picks=picks), n_components=n_comp,
+                             ortho=False, extended=False, max_iter=1000)
+    S_smica /= np.sqrt(np.mean(S_smica ** 2, axis=1, keepdims=True))
+    S_infomax /= np.sqrt(np.mean(S_infomax ** 2, axis=1, keepdims=True))
+    return S, S_smica, S_infomax
+
+
+mi_S = []
+mi_I = []
+n_comps = [20]
+mir_S = []
+mir_I = []
+for n_comp in n_comps:
+    print(n_comp)
+    S, S_smica, S_infomax = get_sources(n_comp)
+    mir_smica = get_mir(S)
+    mi_s = get_mi(S_smica)
+    mi_i = get_mi(S_infomax)
+    mir_s = get_mir(S_smica)
+    mir_i = get_mir(S_infomax)
+# plt.plot(n_comps, [np.max(mi) for mi in mi_S], label='smica')
+# plt.plot(n_comps, [np.max(mi) for mi in mi_I], label='pca')
+# plt.legend()
 # plt.show()
-#
-#
-# # Cluster the spectra
-#
-# labels = smica.plot_clusters(8)
-# plt.show()
-#
-# # Inspect a cluster
-#
-# idx = np.where(labels == 1)[0]
-# smica.plot_components(picks=idx)
-#
-# smica.plot_properties(picks=idx)
