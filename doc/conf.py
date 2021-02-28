9abea4bc8a0b2745770f722169bd5f622e4e6a56
Alexandre Gramfort <alexandre.gramfort@m4x.org>
2021-02-28 21:57:22 +0100

    misc

--- doc/conf.py
+++ doc/conf.py
@@ -276,7 +276,7 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    (master_doc, 'smica', u'Picard Documentation',
+    (master_doc, 'smica', u'SMICA Documentation',
      [author], 1)
 ]
 
